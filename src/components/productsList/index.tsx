2b1a390e01f89fed3c37a85c888f57a29f6b19ae
Luceta <57941164+Luceta@users.noreply.github.com>
2022-03-20 00:11:03 +0900

    🔀Solve: rebase conflict

--- src/components/productsList/index.tsx
+++ src/components/productsList/index.tsx
@@ -26,6 +26,10 @@ const Header = styled.header`
     font-weight: 500;
     font-size: 24px;
     line-height: 35px;
+    margin-bottom: 29px;
+    font-weight: 500;
+    font-size: 24px;
+    line-height: 35px;
   }
 `;
 
