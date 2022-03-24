9bf0f6fca5887d83baaa9541c76508c5d7b862da
junep16 <jiyunpark0816@gmail.com>
2022-03-24 22:54:10 +0900

    ✨ Feat: nickname and checkbox form

--- src/pages/join_info.tsx
+++ src/pages/join_info.tsx
@@ -1,18 +1,49 @@
 import React, { useState } from "react"; 
 import type { NextPage } from "next";
 import styled from "@emotion/styled"
+import Router from "next/router";
 
 const Join: NextPage = () => {
+  const [userNickname, setUserNickname] = useState<string>("");
+  const [checkButton, setCheckButton] = useState<boolean>(false); 
+
+  const handleNickname = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserNickname(e.currentTarget.value);  
+  }
+
+  const handleButton = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault(); 
+    setCheckButton((current)=>!current); 
+  }
+  
+  const onSubmit = () => {
+    if(userNickname) {
+      Router.push("/"); 
+    } else {
+      alert("닉네임을 입력해주세요")
+    }
+  }
 
   return (
     <JoinInfoContainer>
       <JoinForm>
         <h2>추가정보</h2>	
         <label htmlFor="user-nickname"></label>
-        <input type="text" id="user-nickname" placeholder="닉네임을 입력해주세요"/>  
-        {/* {idError && <TextAlert>아이디 또는 비밀번호가 일치하지 않습니다.</TextAlert>}  */}
-        <CheckButton>판매자입니다</CheckButton>
-        <JoinButton type="button">회원가입 완료하기</JoinButton> 
+        <input 
+          type="text" 
+          id="user-nickname" 
+          placeholder="닉네임을 입력해주세요"
+          onChange={handleNickname}
+          value={userNickname}
+        />   
+        <CheckButton
+          check={checkButton}
+          onClick={handleButton}
+        >판매자입니다</CheckButton>
+        <JoinButton 
+          type="button"
+          onClick={onSubmit}
+        >회원가입 완료하기</JoinButton> 
       </JoinForm>
     </JoinInfoContainer>
   )
@@ -57,12 +88,18 @@ const JoinForm = styled.form`
     }
   }
 `
-const CheckButton = styled.button`
+
+interface ContainerTypeProps {
+  check: boolean; 
+}
+const CheckButton = styled.button<ContainerTypeProps>`
   height: 75px;
-  background: url("/assets/check.svg") no-repeat 20px 20px;
+  background: ${(props) => 
+  props.check ? "url('/assets/checked.svg') no-repeat 20px 20px;" : "url('/assets/check.svg') no-repeat 20px 20px;"}; 
   box-sizing: border-box; 
   padding: 10px;
-  background-color: #FFF; 
+  background-color: ${(props) => 
+  props.check ? "#CDC7BF;" : "#FFF;"}; 
   border: 1px solid #847258;  
 `
 const JoinButton = styled.button`
