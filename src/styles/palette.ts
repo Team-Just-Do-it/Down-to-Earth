2a89c4a54c7cd62cf94038637a2998c7e4aecb10
jiseung-kang <ill0o0lli@ajou.ac.kr>
2022-03-24 18:51:56 +0900

    💄 Feat: Add common UI properties

---
+++ src/styles/palette.ts
@@ -0,0 +1,9 @@
+const palette = {
+  normalFont: "000000",
+  lightFont: "#666666",
+  lightBackground: "EBEBEB",
+  mediumBackground: "E5E5E5",
+  highBackground: "#847258",
+};
+
+export default palette;
---
+++ src/styles/sizes.ts
@@ -0,0 +1,9 @@
+const sizes = {
+  smallest: "1.4rem",
+  small: "1.8rem",
+  medium: "2rem",
+  large: "2.4rem",
+  largest: "3rem",
+};
+
+export default sizes;
