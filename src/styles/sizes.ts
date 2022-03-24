775f2ebc06c5355274cfaee29a64cd96d65b5fa1
jiseung-kang <ill0o0lli@ajou.ac.kr>
2022-03-25 01:50:12 +0900

    ♻️ Refactor: Apply review

--- src/styles/sizes.ts
+++
@@ -1,9 +0,0 @@
-const sizes = {
-  smallest: "1.4rem",
-  small: "1.8rem",
-  medium: "2rem",
-  large: "2.4rem",
-  largest: "3rem",
-};
-
-export default sizes;
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -7,6 +7,22 @@ export const size = {
 };
 
 const theme = {
+  palette: {
+    normalFont: "000000",
+    lightFont: "#666666",
+    lightBackground: "EBEBEB",
+    mediumBackground: "E5E5E5",
+    highBackground: "#847258",
+  },
+
+  fontSizes: {
+    smallest: "1.4rem",
+    small: "1.8rem",
+    medium: "2rem",
+    large: "2.4rem",
+    largest: "3rem",
+  },
+
   mainColor: "#847258",
   mediaQuery: {
     laptop: `@media only screen and (min-width: ${size.largest})`,
