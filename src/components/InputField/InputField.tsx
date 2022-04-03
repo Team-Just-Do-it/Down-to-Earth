cf4290911a7381b1dcd7cb2f51cc98198c731543
Luceta <57941164+Luceta@users.noreply.github.com>
2022-04-03 20:20:59 +0900

    ♻️🎨Refactor: page Add style with Inputfield Component

---
+++ src/components/InputField/InputField.tsx
@@ -0,0 +1,41 @@
+import styled from "@emotion/styled";
+import { HTMLInputTypeAttribute } from "react";
+
+interface InputFieldProps {
+  placeholder: string;
+  labelName: string;
+  type: HTMLInputTypeAttribute;
+  controller: any;
+}
+
+export default function InputField({
+  placeholder,
+  labelName,
+  type,
+  controller: { value, onChange },
+}: InputFieldProps) {
+  return (
+    <InputLabel htmlFor={labelName}>
+      <input
+        id={labelName}
+        type={type}
+        placeholder={placeholder}
+        onChange={onChange}
+        autoComplete="on"
+      />
+    </InputLabel>
+  );
+}
+
+const InputLabel = styled.label`
+  input {
+    margin: 10px;
+    padding: 11px 22px;
+    font-weight: 400;
+    font-size: 2rem;
+    line-height: 29px;
+    border: 1px solid #a2a2a2;
+    background: #ffffff;
+    color: #666666;
+  }
+`;
